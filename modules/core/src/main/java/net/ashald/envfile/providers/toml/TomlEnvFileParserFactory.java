4bf16d941e2d120e41589a81829dbfe61870cda3
pedoc <pedoc@qq.com>
2025-10-15 11:12:08 +0800

    feat(toml): 支持文件过滤和显示隐藏文件，添加 getFileNamePredicate 与 showHiddenFiles 方法

--- modules/core/src/main/java/net/ashald/envfile/providers/toml/TomlEnvFileParserFactory.java
+++ modules/core/src/main/java/net/ashald/envfile/providers/toml/TomlEnvFileParserFactory.java
@@ -6,15 +6,20 @@ import net.ashald.envfile.EnvVarsProviderFactory;
 import net.ashald.envfile.providers.EnvFileExecutor;
 import net.ashald.envfile.providers.EnvFileReader;
 import net.ashald.envfile.providers.SingleFileEnvVarsProvider;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class TomlEnvFileParserFactory implements EnvVarsProviderFactory {
     private static final Toml TOML = new Toml();
 
     @Override
-    public EnvVarsProvider createProvider(Map<String, String> baseEnvVars, Consumer<String> logger) {
+    public EnvVarsProvider createProvider(
+            Map<String, String> baseEnvVars,
+            Consumer<String> logger
+    ) {
         return SingleFileEnvVarsProvider.builder()
                 .reader(EnvFileReader.DEFAULT)
                 .executor(EnvFileExecutor.DEFAULT)
@@ -32,6 +37,16 @@ public class TomlEnvFileParserFactory implements EnvVarsProviderFactory {
 
     @Override
     public boolean isEditable() {
+        return true;
+    }
+
+    @Override
+    public @Nullable Predicate<String> getFileNamePredicate() {
+        return name -> name.endsWith(".toml") || name.endsWith(".tml") || name.endsWith(".config");
+    }
+
+    @Override
+    public boolean showHiddenFiles() {
         return true;
     }
 }
